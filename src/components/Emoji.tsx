3721009b3e1b5521a4ef4f278259424785af49eb
sandarminaye <sandarminaye@gmail.com>
2023-04-25 19:59:09 +0700

    added Emoji

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,23 @@
+import bullsEye from "../assets/bullsEye.webp";
+import thumbsUp from "../assets/thumbsUp.webp";
+import meh from "../assets/meh.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  // it's called index synature
+  const emojimap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
+
+  return <Image {...emojimap[rating]} marginTop={1} />;
+};
+
+export default Emoji;
